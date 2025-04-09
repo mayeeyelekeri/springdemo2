59c35a77f77864876da316ee147dfc2f2750facf
Mahesh Mahadeva <yelekeri@gmail.com>
2025-04-08 22:18:54 -0400

    updating buildspec for ecs

--- codedeploy/scripts/start_server.sh
+++ codedeploy/scripts/start_server.sh
@@ -4,5 +4,5 @@ id
 ls -lrt /tmp 
 ls -lrt ~
 echo $HOME
-cd /tmp; docker build -t springdemo2:latest .
-docker run --name springdemo2 -p 8080:8080 -d springdemo2:latest 
+cd /tmp; docker build -t springdemo:latest .
+docker run --name springdemo2 -p 80:80 -d springdemo:latest 
