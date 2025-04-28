471a9adad502116b7c8071693079654242a387bd
Mahesh Mahadeva <yelekeri@gmail.com>
2025-04-28 14:24:52 -0400

    codedeploy

--- codedeploy-ec2/scripts/stop_server.sh
+++ codedeploy-ec2/scripts/stop_server.sh
@@ -2,4 +2,8 @@
 docker ps 
 docker ps -aqf "name=springdemo"  |xargs docker rm -f  | echo "removed any old running containers(if any)"
 docker ps 
+
+ls -lrt /tmp
+rm -rf /tmp/Dockerfile /tmp/*.jar
+ls -lrt /tmp
  
