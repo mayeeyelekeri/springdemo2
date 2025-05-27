1ece9fe77a8df0f4ffec93ab3ab0509b0c3f1ad0
Mahesh Mahadeva <yelekeri@gmail.com>
2025-05-27 19:57:05 -0400

    added hostname to display

--- src/main/java/com/mine/springdemo/MainPageController.java
+++ src/main/java/com/mine/springdemo/MainPageController.java
@@ -27,6 +27,7 @@ public class MainPageController {
 			InetAddress myIP = InetAddress.getLocalHost();
 			System.out.println("system generated IP address: "+ myIP); 
             model.addAttribute("myIP", myIP); 
+            model.addAttribute("myHost", myIP.getHostName()); 
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
